5e8081071972e0c51b2e0effec66ad9493e0b7aa
Ilya Radchenko <knownasilya@gmail.com>
2019-01-26 10:47:09 -0500

    fix(group): change layout to sidebar

---
+++ app/manage/group/controller.ts
@@ -0,0 +1,6 @@
+import Controller from '@ember/controller';
+import Group from 'cg/models/group';
+
+export default class ManageGroupController extends Controller {
+  group!: Group;
+}
--- app/manage/group/route.ts
+++ app/manage/group/route.ts
@@ -1,4 +1,6 @@
 import Route from '@ember/routing/route';
+import Group from 'cg/models/group';
+import Controller from './controller';
 
 interface Params {
   groupId: string;
@@ -9,5 +11,11 @@ export default class ManageGroup extends Route.extend({
 }) {
   model({ groupId }: Params) {
     return this.store.findRecord('group', groupId);
+  }
+
+  setupController(controller: Controller, group: Group) {
+    controller.setProperties({
+      group
+    });
   }
 }
